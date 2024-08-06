d0c5fd7a838330a91a3dbd389c5a3acfc378f726
Contor Andrei <andrei_contor@yahoo.com>
2024-08-06 20:46:26 +0300

    Revert

--- medicament.py
+++ medicament.py
@@ -10,7 +10,6 @@ session = Session()
 # date=2020-12-30 time=10-30-30 and timezone=Europe/London
 Obj1 = datetime.datetime(year=2020, month=5, day=23)
 Obj2 = datetime.datetime(year=2024, month=8, day=20)
-Obj3 = datetime.datetime(year=2024, month=8, day=20)
 
 paracetamol = Medicament(denumire = "Paracetamol", data_expirare = Obj1, pret = 4.2)
 nurofen = Medicament(denumire = "Nurofen", data_expirare = Obj2, pret = 10.3)
@@ -22,11 +21,6 @@ nurofen = Medicament(denumire = "Nurofen", data_expirare = Obj2, pret = 10.3)
 medicamente = session.query(Medicament).order_by(Medicament.data_expirare)
 for medicament in medicamente:
     print(medicament)
-
-
-
-    
-
 
 
 nr_medicamente = session.query(func.count(Medicament.denumire))
